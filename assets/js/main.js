0f5e684850ce1506226cff59c9dda4fc0b0b5e8c
Stefan KP <skp87@me.com>
2017-04-06 11:18:58 +0200

    Update slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,14 @@ $(document).ready(function(){
     arrowShape: {
       x0: 10,
       x1: 60, y1: 50,
-      x2: 65, y2: 45,
-      x3: 20
+      x2: 60, y2: 45,
+      x3: 15
     },
     cellAlign: 'left',
     contain: true,
     dragThreshold: 10,
     imagesLoaded: true,
+    pageDots: false,
     wrapAround: true
   };
 
